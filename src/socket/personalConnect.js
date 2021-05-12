8cb59cbd6b10c765714640d6107f1b0cc9d44fc0
Tochukwu Ozurumba <ozurumbatochukwu@yahoo.com>
2021-05-12 10:03:33 +0100

    :octocat: personal connect dissolved

--- src/socket/personalConnect.js
+++ src/socket/personalConnect.js
@@ -7,16 +7,14 @@ export default async function (socket, io, user){
   // a user joins a room onces
   socket.on('join-chat', async (data) => {
     try {
+      // console.log('here here here', data);
       const { chat_uuid } = data;
-      console.log(`i am in inside the chat room ${chat_uuid}`)
       socket.join(chat_uuid, () => {
         socket.on(`${chat_uuid}-message`, (data) => {
         //   const { sender_uuid, recipient_uuid, parent_uuid, message, } = data;
-
-          sendPrivateMessage(data, io, chat_uuid);
+          sendPrivateMessage(data, io, chat_uuid, user);
         })
       })
-      
     } catch (error) {
         console.log(error);
     }
