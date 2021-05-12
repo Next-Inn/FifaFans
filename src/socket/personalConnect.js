1a78ae4f6a93e216a9ea6f74da417a2f8db8c4bd
Tochukwu Ozurumba <ozurumbatochukwu@yahoo.com>
2021-05-12 09:36:26 +0100

    :octocat: personal connect bug in progress

--- src/socket/personalConnect.js
+++ src/socket/personalConnect.js
@@ -8,6 +8,7 @@ export default async function (socket, io, user){
   socket.on('join-chat', async (data) => {
     try {
       const { chat_uuid } = data;
+      console.log(`i am in inside the chat room ${chat_uuid}`)
       socket.join(chat_uuid, () => {
         socket.on(`${chat_uuid}-message`, (data) => {
         //   const { sender_uuid, recipient_uuid, parent_uuid, message, } = data;
